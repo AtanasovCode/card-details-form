d2462e243ea452d828f0e8f76bf2f9ff660c0287
AtanasovCode <a_ljupce@yahoo.com>
2022-08-30 22:58:43 +0200

    Added Thank You Card

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -13,141 +13,183 @@ const FormValidation = () => {
     const [displayTY, setDisplayTY] = useState(0);
 
     const onSubmitBtn = (e) => {
-        if(
+        if (
             !fullName
             || !cardNumber
-            || !expMonth 
-            || !expYear 
+            || !expMonth
+            || !expYear
             || !CVC
         ) {
             console.log("Some input field is empty");
             setDisplayTY(0);
-        }else {
+        } else {
             setDisplayTY(displayTY + 1);
             console.log("All input fields are full");
         }
     }
 
 
+    const preventLetters = (e) => {
+        if (!/[0-9]/.test(e.key)) {
+            e.preventDefault();
+        }
+    }
+
+    const preventNumbers = (e) => {
+        if (/[0-9]/.test(e.key)) {
+            e.preventDefault();
+        }
+    }
+
+    
+
+    const display = () => {
+        if (displayTY === 0) {
+            return (
+                <div className="full-container">
+                    <div className="left-panel"></div>
+                    <div className="card-front-container">
+                        <CardFacePreview
+                            fullName={fullName}
+                            cardNumber={cardNumber}
+                            expMonth={expMonth}
+                            expYear={expYear}
+                        />
+                    </div>
+                    <div className="card-back-container">
+                        <CardBackPreview CVC={CVC} />
+                    </div>
+                    <div className="right-panel">
+                        <form className="input-container">
+                            <label>
+                                <div>
+                                    Placeholder Name
+                                </div>
+                                <input
+                                    required
+                                    type="text"
+                                    placeholder="e.g Jane Appleseed"
+                                    onChange={(e) => { setFullName(e.target.value) }}
+                                    className="wide-input name-input"
+                                    //Only allow input of letters for full name
+                                    onKeyPress={(e) => preventNumbers(e)}
+                                />
+                            </label>
+                            <label className="card-number-label">
+                                <div>
+                                    Card Number
+                                </div>
+                                <input
+                                    required
+                                    //Max lenght to be 16
+                                    maxLength="16"
+                                    type="text" //Has to be text so maxLength works
+                                    placeholder="e.g 1234 5678 9123 4567"
+                                    onChange={(e) => { setCardNumber(e.target.value) }}
+                                    className="wide-input card-number-input"
+                                    //Function to only allow input of numbers
+                                    onKeyPress={(e) => preventLetters(e)}
+                                />
+                                <div className="card-number-error-text">
+                                    Please Enter Your Full Card Number
+                                </div>
+                            </label>
+                            <label className="card-dates-container">
+                                <div className="card-dates-info">
+                                    <div>
+                                        Exp. Date (MM/YY)
+                                    </div>
+                                    <div className="cvc">
+                                        CVC
+                                    </div>
+                                </div>
+                                <div className="card-dates-input">
+                                    <input
+                                        required
+                                        //Max length to be 2
+                                        maxLength="2"
+                                        type="text"
+                                        placeholder="MM"
+                                        onChange={(e) => { setExpMonth(e.target.value) }}
+                                        className="exp-input"
+                                        //Only allow input of numbers
+                                        onKeyPress={(e) => preventLetters(e)}
+                                    />
+                                    <input
+                                        required
+                                        //Max length to be 2
+                                        maxLength="2"
+                                        type="text"
+                                        placeholder="YY"
+                                        onChange={(e) => { setExpYear(e.target.value) }}
+                                        className="exp-input"
+                                        //Only allow input of numbers
+                                        onKeyPress={(e) => preventLetters(e)}
+                                    />
+                                    <input
+                                        required
+                                        //Max length to be 3
+                                        maxLength="3"
+                                        type="text"
+                                        placeholder="e.g 565"
+                                        onChange={(e) => { setCVC(e.target.value) }}
+                                        className="cvc-input"
+                                        //Only allow input of numbers
+                                        onKeyPress={(e) => preventLetters(e)}
+                                    />
+                                </div>
+                            </label>
+                            <label>
+                                <input
+                                    type="button"
+                                    value="Confirm"
+                                    className="btn"
+                                    onClick={onSubmitBtn}
+                                />
+                            </label>
+                        </form>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="full-container">
+                    <div className="left-panel"></div>
+                    <div className="card-front-container">
+                        <CardFacePreview
+                            fullName={fullName}
+                            cardNumber={cardNumber}
+                            expMonth={expMonth}
+                            expYear={expYear}
+                        />
+                    </div>
+                    <div className="card-back-container">
+                        <CardBackPreview CVC={CVC} />
+                    </div>
+                    <div className="right-panel">
+                        <div className="completed-icon-container">
+                            Icon goes here
+                        </div>
+                        <div>
+                            <h1>THANK YOU!</h1>
+                        </div>
+                        <div>
+                            <button>
+                                CONTINUE
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+
     return (
-        <div className="full-container">
-            <div className="left-panel"></div>
-            <div className="card-front-container">
-                <CardFacePreview 
-                    fullName={fullName}
-                    cardNumber={cardNumber}
-                    expMonth={expMonth}
-                    expYear={expYear}
-                />
-            </div>
-            <div className="card-back-container">
-                <CardBackPreview CVC={CVC} />
-            </div>
-            <div className="right-panel">
-                <form className="input-container">
-                    <label>
-                        <div>
-                            Placeholder Name
-                        </div>
-                        <input
-                            required
-                            type="text"
-                            placeholder="e.g Jane Appleseed"
-                            onChange={(e) => { setFullName(e.target.value) }}
-                            className="wide-input name-input"
-                            //Only allow input of letters for full name
-                            onKeyPress={(e) => preventNumbers(e)}
-                        />
-                    </label>
-                    <label className="card-number-label">
-                        <div>
-                            Card Number
-                        </div>
-                        <input
-                            required
-                            //Max lenght to be 16
-                            maxLength="16"
-                            type="text" //Has to be text so maxLength works
-                            placeholder="e.g 1234 5678 9123 4567"
-                            onChange={(e) => { setCardNumber(e.target.value) }}
-                            className="wide-input card-number-input"
-                            //Function to only allow input of numbers
-                            onKeyPress={(e) => preventLetters(e)}
-                        />
-                        <div className="card-number-error-text">
-                            Please Enter Your Full Card Number
-                        </div>
-                    </label>
-                    <label className="card-dates-container">
-                        <div className="card-dates-info">
-                            <div>
-                                Exp. Date (MM/YY)
-                            </div>
-                            <div className="cvc">
-                                CVC
-                            </div>
-                        </div>
-                        <div className="card-dates-input">
-                            <input
-                                required
-                                //Max length to be 2
-                                maxLength="2"
-                                type="text"
-                                placeholder="MM"
-                                onChange={(e) => { setExpMonth(e.target.value) }}
-                                className="exp-input"
-                                //Only allow input of numbers
-                                onKeyPress={(e) => preventLetters(e)}
-                            />
-                            <input
-                                required
-                                //Max length to be 2
-                                maxLength="2"
-                                type="text"
-                                placeholder="YY"
-                                onChange={(e) => { setExpYear(e.target.value) }}
-                                className="exp-input"
-                                //Only allow input of numbers
-                                onKeyPress={(e) => preventLetters(e)}
-                            />
-                            <input
-                                required
-                                //Max length to be 3
-                                maxLength="3"
-                                type="text"
-                                placeholder="e.g 565"
-                                onChange={(e) => { setCVC(e.target.value) }}
-                                className="cvc-input"
-                                //Only allow input of numbers
-                                onKeyPress={(e) => preventLetters(e)}
-                            />
-                        </div>
-                    </label>
-                    <label>
-                        <input 
-                            type="button"
-                            value="Confirm"
-                            className="btn"
-                            onClick={onSubmitBtn}
-                        />
-                    </label>
-                </form>
-            </div>
+        <div>
+            {display()}
         </div>
     );
-}
-
-
-const preventLetters = (e) => {
-    if (!/[0-9]/.test(e.key)) {
-        e.preventDefault();
-    }
-}
-
-const preventNumbers = (e) => {
-    if (/[0-9]/.test(e.key)) {
-        e.preventDefault();
-    }
 }
 
 export default FormValidation;
